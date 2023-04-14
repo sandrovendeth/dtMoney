17b54fcdfea36bc51cc6e434b3606faa347c18eb
Sandro Vendeth <sandrohenrique92@outlook.com>
2023-04-14 13:58:15 -0300

    14/04

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,16 @@
+import { Container } from "./style";
+
+import { Summary } from "../Summary";
+import { TransactionTable } from "../TransactionTable";
+
+
+
+export function Dashboard() {
+    return(
+        <Container>
+            <Summary />
+            <TransactionTable />
+        </Container>
+
+    );
+}
