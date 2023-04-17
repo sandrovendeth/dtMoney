64e6f70f660c6d61b6ee5c38599c72051f82ab8c
Sandro Vendeth <sandrohenrique92@outlook.com>
2023-04-17 13:39:06 -0300

    17/04

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 Modal.setAppElement('#root');
 
@@ -25,12 +26,12 @@ function handleCloseNewTransactionModal() {
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
-      <Modal isOpen={isNewTransactionModalOpen}
-             onRequestClose={handleCloseNewTransactionModal}>
-                
-                <h2>Cadastrar transação</h2>
-            </Modal> 
-      <GlobalStyle />   
+      
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+        />
+      <GlobalStyle />     
     </>
   );
 }
